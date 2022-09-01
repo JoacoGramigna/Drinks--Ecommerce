608a1427c4d1086e4fb1f03fc7d526333cdfca55
Joaquin Gramigna <joacogramigna@gmail.com>
2022-08-31 23:26:46 -0300

    OnAdd

--- src/components/ItemConter/index.jsx
+++ src/components/ItemConter/index.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import './styles.css';
 
-const ItemConter = ({ initial, stock }) => {
+const ItemConter = ({ initial, stock, onAdd }) => {
     const [contador, setcontador] = useState(initial);
 
     const sumar = () => {
-        if (contador < stock)
+        if (contador < stock) {
             setcontador(contador + 1);
+        } else {
+            alert("No hay suficiente stock");
+        }
     }
     const restar = () => {
-        if (contador > 0)
+        if (contador > 0) {
             setcontador(contador - 1);
+        }
     }
     return (
         <div>
             <button onClick={restar}>-</button>
             <span>{contador}</span>
             <button onClick={sumar}>+</button>
+            <button onClick={() => onAdd(contador)}>Agregar al carrito</button>
         </div>
     )
 }
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ItemConter from '../../components/ItemConter';
 import './styles.css';
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = ({ greeting }) => {
+    const agregarAlCarrito = (cantidad) => {
+        console.log(`Se agregó la cantidad de ${cantidad} productos a tu carrito!`)
+    }
+
     return (
         <div>
             <h1>{greeting}</h1>
-            <ItemConter initial={1} stock={8}/>
+            <ItemConter initial={1} stock={8} onAdd={agregarAlCarrito} />
         </div>
     )
 }
