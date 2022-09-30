5a9c414a09a9a7e4025dc6e75dba498d604f53a2
Joaquin Gramigna <joacogramigna@gmail.com>
2022-09-30 04:13:38 -0300

    Item Collection 2

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,24 +3,46 @@ import './style.css'
 import { Link } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
 import ItemCart from '../ItemCart';
+import OrdenGenerada from '../../services/generarOrden';
+import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
+import {db} from '../../firebase/config';
 
 const Cart = () => {
-    const { cart, cartPrecioTotal, clearCart } = useCart();
+    const { cart, cartPrecioTotal, clearCart, } = useCart();
+
+    const handleBuy = async () => {
+        const total = cartPrecioTotal();
+        const orden = OrdenGenerada("Joaquin", "joacogramigna@gmail.com", 2994013723, cart, total)
+        console.log(orden);
+
+        const docRef = await addDoc(collection(db, "orders"), orden);
+        
+
+        cart.forEach(async (productoEnCarrito)=>{
+            const productRef = doc(db, "productos", productoEnCarrito.id);
+            const productSnap = await getDoc(productRef);
+            await updateDoc(productRef,{
+                stock: productSnap.data().stock - productoEnCarrito.quantity,
+            })
+        })
+        alert(`Compra realizada, ID de compra:  ${docRef.id}`);
+    }
 
     if (cart.length === 0) {
         return (
             <div className='cartVacio'>
-            <p>No hay productos.</p>
-            <Link to='/bebidas' className='linkAgrega'>Agregá productos a tu carrito.</Link>
+                <p>No hay productos.</p>
+                <Link to='/bebidas' className='linkAgrega'>Agregá productos a tu carrito.</Link>
             </div>
         );
     }
 
     return (
         <div className='carro'>
-        {cart.map(product => <ItemCart key={product.id} product={product} />)}
-        <p>Total: ${cartPrecioTotal()}</p>
-        <button onClick={clearCart}>Limpiar carrito</button>
+            {cart.map(product => <ItemCart key={product.id} product={product} />)}
+            <p>Total: ${cartPrecioTotal()}</p>
+            <button onClick={clearCart}>Limpiar carrito</button>
+            <button onClick={handleBuy}>Realizar compra</button>
         </div>
     )
 }
---
+++ src/services/generarOrden.js
@@ -0,0 +1,15 @@
+const OrdenGenerada = (nombre, email, telefono, cart, total) => {
+    return{
+        buyer:{
+            nombre:nombre,
+            email:email,
+            telefono: telefono,
+        },
+        items: cart
+        ,
+        total: total,
+        createdAt: new Date().toLocaleString()
+    }
+}
+
+export default OrdenGenerada;
